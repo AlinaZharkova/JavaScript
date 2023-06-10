a5293c4b36d7f8da169d95f277770956db187732
AlinaZharkova <alinka--91@mail.ru>
2023-06-10 16:13:30 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+/* дз 3 */
+
+const Firstname = prompt("Введите Ваше имя:");
+
+function privet(Firstname) {
+    alert(`Привет, ${Firstname}!`);
+}
+
+privet(Firstname);
+
+
+
+
+/* const name = prompt("Введите Ваше имя:", "Имя");
+alert(`Привет, ${name}`); */
+
+/* const Firstname = prompt("Введите Ваше имя:", "Имя");
+
+function privet(Firstname) {
+    alert(`Привет, ${Firstname}!`);
+}
+
+privet(Firstname); */
+
+/* 
+if (confirm("Вы уверены?")) {
+    alert("Мы рады, что Вы уверены!")
+}
+
+else {
+    alert("Жаль, что Вы не уверены...")
+} */
+
+/* const age = +prompt("Введите Ваш возраст:");
+
+switch (age) {
+    case 10:
+        alert("Вам нужно сделать уроки.")
+        break;
+
+    case 18:
+        alert("Вы совершеннолетний, всё можно!")
+        break;
+
+    case 30:
+        alert("Ложитесь спать, завтра на работу!")
+        break;
+
+    default:
+        alert("Мы не знаем что Вам делать")
+        break;
+} */
